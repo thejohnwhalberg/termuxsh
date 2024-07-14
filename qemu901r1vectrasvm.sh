3b2d44d70cf0543b919341a30c30c61008da5878
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-07-14 19:22:06 +0700

    Add files via upload

--- qemu901r1vectrasvm.sh
+++ qemu901r1vectrasvm.sh
@@ -1,14 +1,20 @@
 apk update
 apk upgrade
 curl -o qemu.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-9.0.1-r1.apk
-curl -o aarch64.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-aarch64-9.0.1-r1.apk
+curl -o aarch64.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-system-aarch64-9.0.1-r1.apk
 curl -o ppc.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-system-ppc-9.0.1-r1.apk
 curl -o x86_64.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-system-x86_64-9.0.1-r1.apk
-curl -o i386.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-i386-9.0.1-r1.apk
+curl -o i386.apk http://dl-cdn.alpinelinux.org/alpine/v3.20/community/aarch64/qemu-system-i386-9.0.1-r1.apk
 apk add --allow-untrusted qemu.apk
 apk add --allow-untrusted aarch64.apk
 apk add --allow-untrusted ppc.apk
 apk add --allow-untrusted x86_64.apk
 apk add --allow-untrusted i386.apk
+rm qemu.apk
+rm aarch64.apk
+rm ppc.apk
+rm x86_64.apk
+rm i386.apk
 apk upgrade
+clear
 qemu-system-x86_64 --version
