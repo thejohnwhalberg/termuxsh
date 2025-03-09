1caa202d73f40a9ae812bedca702f856996c2484
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-03-09 12:44:25 +0700

    Update winxponggcloud.sh

--- winxponggcloud.sh
+++ winxponggcloud.sh
@@ -3,12 +3,20 @@ sudo apt update -y
 sudo apt install apache2 ufw p7zip-full qemu-system-x86-64 -y
 sudo ufw allow 'VNC'
 sudo ufw status
-wget -O file.7z 'https://archive.org/download/windows-xp.-7z_202408/Windows%20XP.7z'
-7za x file.7z
-rm -rf file.7z
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xvzf ngrok-v3-stable-linux-amd64.tgz
-rm -rf ngrok-v3-stable-linux-amd64.tgz
+if [ -e "./XP.qcow2" ]; then
+    echo "File exists"
+else
+    wget -O file.7z 'https://archive.org/download/windows-xp.-7z_202408/Windows%20XP.7z'
+    7za x file.7z
+    rm -rf file.7z
+fi
+if [ -e "./ngrok" ]; then
+    echo "File exists"
+else
+    wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+    tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+    rm -rf ngrok-v3-stable-linux-amd64.tgz
+fi
 ./ngrok authtoken "$token"
 rungrok="$(./ngrok tcp 5900)" &
 clear
@@ -17,3 +25,8 @@ echo "Please go to the following link to check if the generated address is worki
 echo "----------/\----------"
 sleep 5
 sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0
+clear
+echo "To run again, run the following command:"
+echo "----------\/----------"
+echo "rungrok="\$\(./ngrok tcp 5900\)" & sudo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"
+echo "----------/\----------"
