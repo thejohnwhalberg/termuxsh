025d10b27a3ffcfb737b6f3c81144db10877655d
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-03-09 12:31:23 +0700

    Update winxponggcloud.sh

--- winxponggcloud.sh
+++ winxponggcloud.sh
@@ -3,14 +3,14 @@ sudo apt update -y
 sudo apt install apache2 ufw p7zip-full qemu-system-x86-64 -y
 sudo ufw allow 'VNC'
 sudo ufw status
+wget -O file.7z 'https://archive.org/download/windows-xp.-7z_202408/Windows%20XP.7z'
+7za x file.7z
+rm -rf file.7z
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xvzf ngrok-v3-stable-linux-amd64.tgz
 rm -rf ngrok-v3-stable-linux-amd64.tgz
 ./ngrok authtoken "$token"
 rungrok="$(./ngrok tcp 5900)" &
-wget -O file.7z 'https://archive.org/download/windows-xp.-7z_202408/Windows%20XP.7z'
-7za x file.7z
-rm -rf file.7z
 clear
 echo "----------\/----------"
 echo "Please go to the following link to check if the generated address is working and connect using VNC: https://dashboard.ngrok.com/agents"
