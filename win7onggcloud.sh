7b49087c581d8f8eee5d33be2e7399e00589060a
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-06-01 20:47:04 +0700

    Update win7onggcloud.sh

--- win7onggcloud.sh
+++ win7onggcloud.sh
@@ -28,7 +28,7 @@ echo 'echo "The address to connect to via VNC is below. If you do not see anythi
 echo "echo " >> start7.sh
 echo 'echo "-"' >> start7.sh
 echo "echo " >> start7.sh
-echo "curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE \"s/.*public_url\":\"tcp:\/\/\([^\\\"]*\).*/\\1/p\"" >> start7.sh
+echo "curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url\":\"tcp:..([^\"]*).*/\1/p'" >> start7.sh
 echo "sleep 10" >> start7.sh
 echo "sudo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"  >> start7.sh
 echo "clear" >> start7.sh
