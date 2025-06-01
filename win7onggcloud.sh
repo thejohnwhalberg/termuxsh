94b04acb736958a5fe75dc5bb78a2ddca1995d28
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-06-01 20:23:53 +0700

    Update win7onggcloud.sh

--- win7onggcloud.sh
+++ win7onggcloud.sh
@@ -19,18 +19,20 @@ else
     rm -rf ngrok-v3-stable-linux-amd64.tgz
 fi
 ./ngrok authtoken "$token"
-rungrok="$(./ngrok tcp 5900)" &
-clear
-echo "The address to connect to via VNC is below. If you don't see anything below, try checking here: https://dashboard.ngrok.com/agents"
-echo ""
-echo "-"
-echo ""
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-sleep 10
-sudo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0
-clear
-echo "To run again, run the following command:"
-echo ""
-echo "-"
-echo ""
-echo "rungrok="\$\(./ngrok tcp 5900\)" & sudo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"
+echo "rungrok="$(./ngrok tcp 5900)" &" >> start7.sh
+echo "clear" > start7.sh
+echo "The address to connect to via VNC is below. If you don't see anything below, try checking here: https://dashboard.ngrok.com/agents" >> start7.sh
+echo "echo " >> start7.sh
+echo "echo "-"" >> start7.sh
+echo "echo " >> start7.sh
+echo "curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'" >> start7.sh
+echo "sleep 10" >> start7.sh
+echo "sudo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0"  >> start7.sh
+echo "clear" >> start7.sh
+echo "To run again, run the following command:" >> start7.sh
+echo "echo " >> start7.sh
+echo "echo "-"" >> start7.sh
+echo "echo " >> start7.sh
+echo "./start7" >> start7.sh
+chmod +x start7.sh
+./start7.sh
