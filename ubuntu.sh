73815b77d7a4d60bf1432bcea8e2f5f657b4ca8d
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-06-02 23:27:52 +0700

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,7 @@
+clear
+echo "The owner of this script will not be responsible if any unexpected problems occur whether you edit this script or not. It will start in 10 seconds and you agree to this. To cancel press Ctrl + C."
+sleep 10
+clear
 echo You are here! It wont take long. Just a moment...
 echo Installing X11-repo...
 pkg install x11-repo
