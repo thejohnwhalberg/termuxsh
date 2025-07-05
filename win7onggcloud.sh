13c61ae6d102ca4b8835266c9928d24c35495ca1
thejohnwhalberg <whalberg@jvs.edu.vn>
2025-07-05 14:17:46 +0800

    Update win7onggcloud.sh

--- win7onggcloud.sh
+++ win7onggcloud.sh
@@ -20,17 +20,26 @@ else
 fi
 clear
 
-# This section is for starting the QEMU and then the Pinggy tunnel
+# This section starts QEMU and then waits for the VNC port to be ready before starting Pinggy
 echo "clear" > start7.sh
 echo 'echo "Starting QEMU in the background..."' >> start7.sh
 echo "sudo qemu-system-x86_64 -audiodev pa,id=snd0 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0 &"  >> start7.sh
-echo 'echo "Waiting for QEMU to initialize..."' >> start7.sh
-echo "sleep 15" >> start7.sh
+
+# This loop waits for the VNC port (5900) to be open before continuing
+echo 'echo "Waiting for VNC server to initialize..."' >> start7.sh
+echo 'while ! sudo ss -tln | grep -q ":5900" ; do' >> start7.sh
+echo '    echo -n "." ' >> start7.sh
+echo '    sleep 2' >> start7.sh
+echo 'done' >> start7.sh
+echo 'echo ""' >> start7.sh
+echo 'echo "VNC server is ready!"' >> start7.sh
+
 echo "clear" >> start7.sh
 echo 'echo "Starting Pinggy tunnel..."' >> start7.sh
 echo 'echo "The address to connect to via VNC will be provided by Pinggy below:"' >> start7.sh
 echo "echo " >> start7.sh
 echo 'ssh -p 443 -R0:127.0.0.1:5900 qr@free.pinggy.io' >> start7.sh
+
 echo "clear" >> start7.sh
 echo 'echo "To run again, run the following command:"' >> start7.sh
 echo "echo " >> start7.sh
