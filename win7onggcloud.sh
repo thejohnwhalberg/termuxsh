4adc0887b27b15d6e9d257aa3e4f1d11244ca476
thejohnwhalberg <whalberg@jvs.edu.vn>
2025-07-05 14:25:45 +0800

    Update win7onggcloud.sh

--- win7onggcloud.sh
+++ win7onggcloud.sh
@@ -7,7 +7,7 @@ echo
 echo "------------------------------------------------"
 echo
 sudo apt update -y
-sudo apt install apache2 ufw p7zip-full aria2 qemu-system-x86-64 -y
+sudo apt install apache2 ufw p7zip-full aria2 qemu-system-x86-64 ss-tln -y
 sudo ufw allow 'VNC'
 sudo ufw status
 clear
@@ -23,7 +23,7 @@ clear
 # This section starts QEMU and then waits for the VNC port to be ready before starting Pinggy
 echo "clear" > start7.sh
 echo 'echo "Starting QEMU in the background..."' >> start7.sh
-# The -audiodev flag has been replaced with -no-audio to completely disable audio.
+# This is the corrected line that completely disables audio
 echo "sudo qemu-system-x86_64 -no-audio -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 2048M -drive file=Tiny7.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :0 &"  >> start7.sh
 
 # This loop waits for the VNC port (5900) to be open before continuing
